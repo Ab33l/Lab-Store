efa7868c9e33c3f64c37e47143c88422a14cce2f
Ab33l <abell.wandili@strathmore.edu>
2023-06-13 22:21:28 +0300

    Get request for the Access Token

--- Lab A/Daraja - Node Implementation/app.js
+++ Lab A/Daraja - Node Implementation/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const body_parser = require('body-parser');
 const app = express();
+const request = require('request');
 app.use(body_parser.json());
 
 //routes
@@ -8,5 +9,32 @@ app.get('/', (req, res) =>{
     res.send('App Set up Complete');
 });
 
+//Get Access Token
+app.get('/access_token', (req, res) => {
+    let url = "";
+    let auth = new Buffer().toString('base64');
+
+    request({
+        url: "",
+        headers: {
+            "Authorization": "Basic " + auth
+        }
+       },
+        (error, response, body) => {
+            if(err){
+                console.log(error)
+            }else{
+                res.status(200).json(body);
+            }
+        }
+    )
+});
+
 //listen
-app.listen(5000);
+app.listen(5000, (err, live) =>{
+    if(err){
+        console.error(err);
+    }
+
+    console.log("Server running on port 5000");
+});
