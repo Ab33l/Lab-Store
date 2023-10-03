7fbafa0fbc351a586a8494405ee7a4c3405594f6
Ab33l <abell.wandili@strathmore.edu>
2023-10-03 12:21:17 +0300

    Adjustments to the comments tag which commented the full code

--- Lab B/Blind 75/Container with most water/containermost.py
+++ Lab B/Blind 75/Container with most water/containermost.py
@@ -1,7 +1,6 @@
-'''
-Input => height = [1,8,6,2,5,4,8,3,7]
-Output => 49
-'''
+#Input => height = [1,8,6,2,5,4,8,3,7]
+#Output => 49
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         startLen = 0
--- Lab B/Blind 75/Contains duplicates/containsduplicates.py
+++ Lab B/Blind 75/Contains duplicates/containsduplicates.py
@@ -1,7 +1,7 @@
-'''
-Input => nums = [1,1,1,3,3,4,3,2,4,2]
-Output => true
-'''
+#Input => nums = [1,1,1,3,3,4,3,2,4,2]
+#Output => true
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         newlist = set()
