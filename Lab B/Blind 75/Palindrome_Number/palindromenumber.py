eabd79aaa34ba6bbd0b48ba7b2d42ee3e38cd2d9
Abel Wasike <32242176+Ab33l@users.noreply.github.com>
2024-05-05 23:25:52 +0300

    Create palindromenumber.py
    Check if a number is a palindrome

---
+++ Lab B/Blind 75/Palindrome_Number/palindromenumber.py
@@ -0,0 +1,22 @@
+Input: x = 121
+Output: True, 121 = 121
+
+Input: x = -121
+Output: False, 121- != 121
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        div = 1
+        while x >= 10 * div:
+            div *= 10
+
+        while x:
+            right = x % 10
+            left = x // div
+
+            if left != right: return False
+
+            x = (x % div) // 10
+            div = div / 100
+        return True
