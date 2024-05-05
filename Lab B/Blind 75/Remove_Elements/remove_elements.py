b8a90377f070b9b60fc6ca1ae563fe9db0ec8468
Abel Wasike <32242176+Ab33l@users.noreply.github.com>
2024-05-05 23:34:59 +0300

    Update remove_elements.py
    Adding Comments

--- Lab B/Blind 75/Remove_Elements/remove_elements.py
+++ Lab B/Blind 75/Remove_Elements/remove_elements.py
@@ -1,8 +1,8 @@
-Input: nums = [3,2,2,3], val = 3
-Output: 2, nums = [2,2,_,_]
+#Input: nums = [3,2,2,3], val = 3
+#Output: 2, nums = [2,2,_,_]
 
-Input: nums = [0,1,2,2,3,0,4,2], val = 2
-Output: 5, nums = [0,1,4,0,3,_,_,_]
+#Input: nums = [0,1,2,2,3,0,4,2], val = 2
+#Output: 5, nums = [0,1,4,0,3,_,_,_]
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
