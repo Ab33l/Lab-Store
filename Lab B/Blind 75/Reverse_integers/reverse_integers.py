374c04621783b33b886d94754884634ebeb49515
Abel Wasike <32242176+Ab33l@users.noreply.github.com>
2024-05-09 23:56:06 +0300

    Reverse integers, reverse order of element numbers

---
+++ Lab B/Blind 75/Reverse_integers/reverse_integers.py
@@ -0,0 +1,19 @@
+'''
+Input: x = 123
+Output: 321
+Input: x = -123
+Output: -321
+Input: x = 120
+Output: 21
+'''
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        if x < 0:
+            res = int(str(x)[1:][::-1]) * -1
+        else:
+            res = int(str(x)[::-1])
+        if res > 2 ** 31 - 1 or res < -2 ** 31:
+            return 0
+        return res
+        
